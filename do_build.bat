619ce366bc3fa53f2386587d9df8fbaa4fddf4f1
Roxanne <35779969+CyanSorcery@users.noreply.github.com>
2025-06-18 14:10:33 -0700

    more work on polishing upload scripts

--- do_build.bat
+++ do_build.bat
@@ -1,8 +1,9 @@
 @echo off
 call do_compression.bat
 echo Minifying cart...
-..\shrinko\shrinko8.exe -f bin -F p8 -M --title "blobun mini" --title "by cyansorcery" blobun_mini.p8 blobun_mini.bin
+rem ..\shrinko\shrinko8.exe -f bin -F p8 -M blobun_mini.p8 blobun_mini.bin --pico8-dat "C:\Program Files (x86)\PICO-8\pico8.dat"
+..\shrinko\shrinko8.exe -f png -F p8 -M --title "blobun mini" --title "by cyansorcery" blobun_mini.p8 blobun_mini.p8.png
 echo Done. Exporting desktop binary...
-start /wait pico8.exe blobun_mini.min.p8 -export "-e readme.txt blobun_mini.bin"
+start /wait pico8.exe blobun_mini.p8.png -export "-e readme.txt blobun_mini.bin"
 echo Exporting HTML
-start /wait pico8.exe blobun_mini.min.p8 -export "blobun_mini_html/blobun_mini.html"
+start /wait pico8.exe blobun_mini.p8.png -export "blobun_mini_html/index.html"
--- itch_upload.bat
+++ itch_upload.bat
@@ -4,5 +4,6 @@ butler push blobun_mini.bin/blobun_mini_windows.zip cyansorcery/blobun-mini:wind
 butler push blobun_mini.bin/blobun_mini_osx.zip cyansorcery/blobun-mini:osx
 butler push blobun_mini.bin/blobun_mini_linux.zip cyansorcery/blobun-mini:linux
 butler push blobun_mini.bin/blobun_mini_raspi.zip cyansorcery/blobun-mini:raspi
+butler push blobun_mini.p8.png cyansorcery/blobun-mini:pico8
 butler push blobun_mini_html cyansorcery/blobun-mini:html
 echo Done
