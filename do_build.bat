98a0647e3eaa32dd4b514a76186ecb423f13f8de
Roxanne <35779969+CyanSorcery@users.noreply.github.com>
2025-07-14 14:52:46 -0700

    fix build script

--- do_build.bat
+++ do_build.bat
@@ -2,7 +2,7 @@
 call do_compression.bat
 echo Minifying cart...
 rem ..\shrinko\shrinko8.exe -f bin -F p8 -M blobun_mini.p8 blobun_mini.bin --pico8-dat "C:\Program Files (x86)\PICO-8\pico8.dat"
-..\shrinko\shrinko8.exe -f png -F p8 -M --title "blobun mini" --title "by cyansorcery" blobun_mini.p8 blobun_mini.p8.png
+..\shrinko\shrinko8.exe -f png -F p8 -m --title "blobun mini" --title "by cyansorcery" blobun_mini.p8 blobun_mini.p8.png
 echo Done. Exporting desktop binary...
 start /wait pico8.exe blobun_mini.p8.png -export "-e res/readme.txt blobun_mini.bin"
 echo Exporting HTML
